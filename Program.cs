cb83f15fb7bd6963772c910e68a2ef15194b8719
Ramazan BOZKURT <ramazanbozkurt-20@hotmail.com>
2020-11-10 18:49:09 +0300

    hata

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Yeni_konsol
+{
+    class Program
+    {
+        public class ShopContext : DbContext
+        {
+            public DbSet<Product> Products { get; set; }
+            public DbSet<Category> Categories { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseMySql("server=localhost; port=3306; database=ShopDb; user=root; password=15r!bozkurt2?0");
+            }
+        }
+        public class Product 
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+        }
+
+        public class Category
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
